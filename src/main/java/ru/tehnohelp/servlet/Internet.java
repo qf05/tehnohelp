9e349ce4ff1d682d1ee3fc4f54abf627ef098f2b
Evgenii <35301866+qf05@users.noreply.github.com>
2022-03-09 13:43:44 +0300

    inet

--- src/main/java/ru/tehnohelp/servlet/Internet.java
+++ src/main/java/ru/tehnohelp/servlet/Internet.java
@@ -7,16 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/learning")
-public class Learning extends HttpServlet {
+@WebServlet("/internet")
+public class Internet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/learning.html").forward(req, resp);
+        req.getRequestDispatcher("/internet.html").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/learning.html").forward(req, resp);
+        req.getRequestDispatcher("/internet.html").forward(req, resp);
     }
 }
--- src/main/java/ru/tehnohelp/servlet/Tv.java
+++ src/main/java/ru/tehnohelp/servlet/Tv.java
@@ -7,16 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/tv_and_internet")
-public class TvAndInternet extends HttpServlet {
+@WebServlet("/tv")
+public class Tv extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/internet.html").forward(req, resp);
+        req.getRequestDispatcher("/tv.html").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/internet.html").forward(req, resp);
+        req.getRequestDispatcher("/tv.html").forward(req, resp);
     }
 }
